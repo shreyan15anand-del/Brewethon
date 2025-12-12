213d158e1f7983db032647d15eddc2f076dff8e3
shreyan <shreyan@shreyans-MacBook-Pro.local>
2025-12-12 15:00:21 +0530

    Fix async pre-save hook; add /admin/setup-default

--- models/Admin.js
+++ models/Admin.js
@@ -7,18 +7,13 @@ const AdminSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Normalize email and hash password before saving
-AdminSchema.pre('save', async function(next) {
+AdminSchema.pre('save', async function() {
   // lowercase email for consistent lookups
   if (this.email) this.email = this.email.toLowerCase();
 
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Instance method to compare password
